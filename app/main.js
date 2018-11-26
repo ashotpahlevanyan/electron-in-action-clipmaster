df88e26c3692c1a175981a2024c27929e8c05863
Ashot Pahlevanyan <ashotpahlevanyan@gmail.com>
2018-11-26 11:09:41 +0400

    added dummy tray and hidden the dock

---
+++ app/main.js
@@ -0,0 +1,28 @@
+const path = require('path');
+const {
+	app,
+	Menu,
+	Tray
+} = require('electron');
+
+let tray = null;
+
+app.on('ready', () => {
+	tray = new Tray(path.join(__dirname, '/Icon.png'));
+	if(process.platform === 'win32') {
+		tray.on('click', tray.popUpContextMenu);
+	}
+
+	if(app.dock) { app.dock.hide(); }
+	const menu = Menu.buildFromTemplate([
+		{
+			label: 'Quit',
+			click() { app.quit(); }
+		}
+	]);
+
+	tray.setToolTip('Clipmaster');
+	tray.setContextMenu(menu);
+});
+
+
