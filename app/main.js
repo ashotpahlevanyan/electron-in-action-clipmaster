52e12873335501e49dfe44b0fcaddd2bf26a5e30
Ashot Pahlevanyan <ashotpahlevanyan@gmail.com>
2018-11-26 11:17:17 +0400

    added tray icon depending on windows, and mac dark or light mode

--- app/main.js
+++ app/main.js
@@ -2,13 +2,24 @@ const path = require('path');
 const {
 	app,
 	Menu,
-	Tray
+	Tray,
+	systemPreferences
 } = require('electron');
+
+const getIcon = () => {
+	if(process.platform === 'win32') {
+		return 'icon-light@2x.ico';
+	}
+	if(systemPreferences.isDarkMode()) {
+		return 'icon-light.png';
+	}
+	return 'icon-dark.png';
+};
 
 let tray = null;
 
 app.on('ready', () => {
-	tray = new Tray(path.join(__dirname, '/Icon.png'));
+	tray = new Tray(path.join(__dirname, getIcon()));
 	if(process.platform === 'win32') {
 		tray.on('click', tray.popUpContextMenu);
 	}
